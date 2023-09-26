5823ee4ff05b40fcfee9ee50feb2729a8ef8daf6
Javohir <javohirpython@gmail.com>
2023-09-26 10:08:27 +0500

    some changed

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / "static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
